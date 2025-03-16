45e89baedc5a938ac4305d08345271e55eaa36d6
GANGANI <gangani95ariyarathne@gmail.com>
2025-03-15 20:20:01 -0400

    Add crawls

--- src/v2-bt-news-collector.py
+++ src/v2-bt-news-collector.py
@@ -89,7 +89,7 @@ def is_news_article(link):
     link = get_expanded_url(link)
 
     if not is_valid_url(link):
-        print(f"Invalid URL: {link}")
+        logging.info(f"Invalid URL: {link} for is_news_article")
         return is_news_article
 
     parsed_url = urlparse(link)
@@ -112,39 +112,39 @@ def is_news_article(link):
         if count > 20:
             is_news_article = True
         else:
-            print(f"Word count is less for {link}\n {plaintext}\n")
+            logging.info(f"Word count is less for {link}\n {plaintext}\n")
             is_news_article = False
     except Exception as e:
         # If an exception occurs, write the error message to the log file
-        print(f"Error processing link: {link} because of {e}\n")
+        logging.error(f"Error processing link: {link} because of {e}\n")
         is_news_article = False
 
     return is_news_article
 
 
-def get_archived_path(link, directory, website_hash):
+def get_archived_path(link, directory):
     """Archive a URL using Browsertrix and return the archived file path."""
     try:
         os.makedirs(directory, exist_ok=True)
+        url_hash = hashlib.md5(website_url.encode()).hexdigest()
+        command = f"docker run -v $PWD/{directory}:/crawls/ -it webrecorder/browsertrix-crawler crawl --url {link} --generateWACZ --collection {url_hash}"
         
-        command = f"docker run -v $PWD/{directory}:/crawls/ -it webrecorder/browsertrix-crawler crawl --url {link} --generateWACZ --collection {website_hash}"
-        
-        print(f"Starting subprocess: {command} with live logging...")
+        logging.info(f"Starting subprocess: {command} with live logging...")
 
         # Open subprocess with real-time logging
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         # Read output line by line
         for line in process.stdout:
-            print(line, end="")  # Print logs in real time
+            print(line, end="")  
 
         for line in process.stderr:
-            print(line, end="")  # Capture and print errors as well
+            print(line, end="")  
 
         process.wait()  # Wait for process to finish
 
         # Check if the archive file was created
-        archive_path = os.path.join(directory, f"{website_hash}.wacz")
+        archive_path = os.path.join(directory, "collection", url_hash)
         if os.path.exists(archive_path):
             logging.info(f"Archived URL {link} at {archive_path}")
             return archive_path
@@ -155,7 +155,6 @@ def get_archived_path(link, directory, website_hash):
     except subprocess.SubprocessError as e:
         logging.error(f"Exception during archiving {link}: {e}")
         return None
-
 
 
 def save_to_file(filepath, data, mode='at'):
@@ -177,51 +176,34 @@ def save_to_file(filepath, data, mode='at'):
         logging.error(f"Error saving data to {filepath}: {e}")
 
 
-def read_cached_urls(filepath):
-    """Read cached URLs from a gzip file."""
-    if os.path.exists(filepath):
-        try:
-            with gzip.open(filepath, "rt", encoding="utf-8") as f:
-                return {line.strip() for line in f}
-        except Exception as e:
-            logging.error(f"Error reading cache file {filepath}: {e}")
-    return set()
-
-
-def save_publication(state, year, month, date, website_url, publication):
-    website_hash = hashlib.md5(website_url.encode()).hexdigest()
-    directory_path = os.path.join("news", state, str(year), str(month), str(date), str(website_hash))
-    os.makedirs(directory_path, exist_ok=True)
-
-    wesite_file_path = os.path.join(directory_path, f"{website_hash}.jsonl.gz")
-    if not os.path.exists(wesite_file_path):
-        logging.info(f"Website: {website_url} has been saved")
-        archived_url = get_archived_path(website_url)
-        if archived_url:
-            publication['archived_link'] = archived_url
-            with gzip.open(wesite_file_path, "at") as f:
-                f.write(json.dumps(publication))
-
-
 # Main Processing
 def process_publication(state, publication, year, month, day):
     """Process a single publication and save its articles."""
+
     website_url = publication.get("website")
     logging.info(f"Processing publication: {website_url}")
+
     rss_feeds = publication.get("rss", [])
     website_hash = hashlib.md5(website_url.encode()).hexdigest()
     directory = os.path.join("news", state, str(year), str(month), str(day), website_hash)
-    cache_filepath = os.path.join(directory, f"{website_hash}-cache.txt.gz")
-    cached_urls = read_cached_urls(cache_filepath)
-    website_json = None
-    archived_website_path = get_archived_path(website_url, directory, website_hash)
-    if archived_website_path:
-        website_json = {
-            'website_link': website_url,
-            'publication_metadata': publication,
-            'archived_time': datetime.datetime.now().isoformat(),
-            'archived_path': archived_website_path
-        }
+
+    metadata_file_path = os.path.join(directory, f"{website_hash}_metadata.jsonl.gz")
+
+    # Check if the file already exists
+    if not os.path.exists(metadata_file_path):
+        archived_website_path = get_archived_path(website_url, directory)
+        if archived_website_path:
+            website_json = {
+                'website_link': website_url,
+                'publication_metadata': publication,
+                'archived_time': datetime.datetime.now().isoformat(),
+                'archived_path': archived_website_path
+            }
+            # Save to file (append if the file doesn't exist yet)
+            save_to_file(metadata_file_path, website_json, 'at')
+            logging.info(f"Metadata of the website: {website_url} is successfully updated in the location: {metadata_file_path}")
+    else:
+        logging.info(f"File {metadata_file_path} already exists, skipping save.")
 
     article_json_objs = []
     nlinks = 0
@@ -232,9 +214,9 @@ def process_publication(state, publication, year, month, day):
         feed = feedparser.parse(rss_feed_url)
         for entry in feed.entries:
             article_url = entry.link
-            if article_url not in cached_urls and is_news_article(article_url):
+            if article_url and is_news_article(article_url):
                 logging.info(f"Found article: {article_url}")
-                archived_path = get_archived_path(article_url, directory, website_hash)
+                archived_path = get_archived_path(article_url, directory)
                 if archived_path:
                     article_json_objs.append({
                         'link': article_url,
@@ -242,7 +224,6 @@ def process_publication(state, publication, year, month, day):
                         'archived_time': datetime.datetime.now().isoformat(),
                         'archived_path': archived_path
                     })
-                    cached_urls.add(article_url)
                     nlinks += 1
                     if nlinks >= 5:
                         break
@@ -257,9 +238,9 @@ def process_publication(state, publication, year, month, day):
             response = requests.get(website_url, headers=HEADERS, timeout=10)
             response.raise_for_status()
             for article_url in extract_article_urls_from_html(response.text, website_url):
-                if article_url not in cached_urls and is_news_article(article_url):
+                if article_url and is_news_article(article_url):
                     logging.info(f"Found article: {article_url}")
-                    archived_path = get_archived_path(article_url, directory, website_hash)
+                    archived_path = get_archived_path(article_url, directory)
                     if archived_path:
                         article_json_objs.append({
                             'link': article_url,
@@ -267,7 +248,6 @@ def process_publication(state, publication, year, month, day):
                             'archived_time': datetime.datetime.now().isoformat(),
                             'archived_path': archived_path
                         })
-                        cached_urls.add(article_url)
                         nlinks += 1
                         if nlinks >= 5:
                             break
@@ -275,10 +255,10 @@ def process_publication(state, publication, year, month, day):
         except requests.RequestException as e:
             logging.error(f"Error scraping {website_url}: {e}")
 
-    # Save Results
-    save_to_file(os.path.join(directory, f"{website_hash}.jsonl.gz"), article_json_objs, 'at')
-    save_to_file(os.path.join(directory, f"{website_hash}_metadata.jsonl.gz"), website_json, 'at')
-    save_to_file(cache_filepath, '\n'.join(cached_urls), 'wt')
+    website_article_location = os.path.join(directory, f"{website_hash}_articles.jsonl.gz")
+    logging.info(f"Articles of the website: {website_url} is successfully updated in the location: {website_article_location}")
+    save_to_file(website_article_location, article_json_objs, 'at')
+    
 
 # Function to get the status code of a URL
 def get_status_code(url):
@@ -286,7 +266,7 @@ def get_status_code(url):
         response = requests.get(url, timeout=10, allow_redirects=True)
         return response.status_code
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching {url}: {e}")
+        logging.info(f"Error fetching {url}: {e}")
         return None  # In case of error, return None
 
 # Run the Script
@@ -297,9 +277,10 @@ while True:
             for publication in publications.get(news_media, []):
                 website_url = publication.get("website")
                 response_status = get_status_code(website_url)
-                logging.info(response_status)
+                logging.info(f"The response status of {website_url} is: {response_status}")
                 if response_status and (200 <= response_status < 300):
                     timestamp = datetime.datetime.now()
+                    logging.info(f"The response status of {website_url} is: {response_status}")
                     process_publication(state, publication, timestamp.year, timestamp.month, timestamp.day)
                     save_publication(state, timestamp.year, timestamp.month, timestamp.day, website_url, publication)
     time.sleep(1)  # Prevent overwhelming the server
